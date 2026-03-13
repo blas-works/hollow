e5c9a8194e0ebc8e3f64be12dd7b92f76fd2b253
Luis Torres <torrescereno@gmail.com>
2026-03-12 22:00:26 -0300

    fix: delete migrations

--- resources/migrations/0001_wooden_supreme_intelligence.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `streaks` (
-	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-	`current_streak` integer DEFAULT 0 NOT NULL,
-	`best_streak` integer DEFAULT 0 NOT NULL,
-	`last_activity_date` integer,
-	`updated_at` integer
-);
